4a23269c5b32abd2cd610187d059c76ecbd3f7bb
azmatdeveloper10 <101882759+azmatdeveloper10@users.noreply.github.com>
2023-01-19 21:50:44 +0500

    second commit

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,10 +1,20 @@
-import { Grid, Box, Typography, TextField, IconButton } from '@mui/material';
+import {
+  Grid,
+  Box,
+  Typography,
+  TextField,
+  IconButton,
+  Button,
+  MenuItem,
+  InputLabel,
+} from '@mui/material';
 import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import {
   List,
   ListItem,
   ListItemAvatar,
   Avatar,
+  Select,
   ListItemText,
 } from '@mui/material';
 import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
@@ -12,31 +22,55 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import { useState } from 'react';
 import { borderRadius } from '@mui/system';
 import { indigo } from '@mui/material/colors';
+import Checkbox from '@mui/material/Checkbox';
+
+import FormControl from '@mui/material/FormControl';
 
 const Form = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState({
+    Task: '',
+    Desc: '',
+    status: '',
+    periority: '',
+  });
   const [inputError, setError] = useState('');
   const [tasks, setTasks] = useState([]);
   const [completedTasks, setCompletedTasks] = useState([]);
 
   const formSub = function (e) {
     e.preventDefault();
-    if (input.length <= 5) {
-      setTasks(prevTask => [...prevTask]);
-    } else {
-      setTasks(prevTask => [...prevTask, { id: Date.now(), title: input }]);
-      setInput('');
+    if (input.status === 'incomplete') {
+      setTasks(prevTask => {
+        return [...prevTask, input];
+      });
+    } else if (input.status === 'complete') {
+      setCompletedTasks(prevCom => {
+        return [...prevCom, input];
+      });
     }
   };
 
-  console.log(tasks);
+  const handleInput = e => {
+    // e.target.value.length <= 5
+    //   ? setError('input must be greater then 5 chracter')
+    //   : setError('');
+    // setInput(prevInput => {
+    //   return {
+    //     ...prevInput,
+    //     [e.target.name]: e.target.value,
+    //   };
+    // });
 
-  const handleInput = function (e) {
-    e.target.value.length <= 5
-      ? setError('input must be greater then 5 chracter')
-      : setError('');
-    setInput(e.target.value);
+    setInput(prevInput => {
+      return {
+        ...prevInput,
+        id: Date.now(),
+        [e.target.name]: e.target.name === 'checked' ? true : e.target.value,
+      };
+    });
   };
+
+  console.log(input);
 
   const completedTask = function (task) {
     const taskCopy = tasks.slice(0);
@@ -81,7 +115,7 @@ const Form = () => {
             <Typography variant='h3' color='primary' fontSize={40} mb={2}>
               React todo list App
             </Typography>
-            <Grid container justifyContent={'center'}>
+            <Grid container justifyContent={'center'} gap={3}>
               <Grid item xs={8}>
                 <TextField
                   id='outlined-basic'
@@ -89,11 +123,70 @@ const Form = () => {
                   variant='outlined'
                   fullWidth={true}
                   size='small'
-                  value={input}
+                  name='Task'
+                  value={input.Task}
                   onChange={handleInput}
                   error={inputError ? true : false}
                   helperText={inputError}
                 />
+              </Grid>
+
+              <Grid item xs={8}>
+                <TextField
+                  id='outlined-basic'
+                  label='Add Description'
+                  variant='outlined'
+                  fullWidth={true}
+                  size='small'
+                  name='Desc'
+                  value={input.Desc}
+                  onChange={handleInput}
+                  error={inputError ? true : false}
+                  helperText={inputError}
+                />
+              </Grid>
+
+              <Grid item xs={8}>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <FormControl style={{ width: '150px' }} size='small'>
+                      <InputLabel id='demo-simple-select-label'>
+                        Status
+                      </InputLabel>
+                      <Select
+                        labelId='demo-simple-select-label'
+                        id='demo-simple-select'
+                        label='Status'
+                        name='status'
+                        value={input.status}
+                        onChange={handleInput}
+                      >
+                        <MenuItem value={'complete'}>Complete</MenuItem>
+                        <MenuItem value={'incomplete'}>Incomplete</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <FormControl style={{ width: '150px' }} size='small'>
+                      <InputLabel id='demo-simple-select-label'>
+                        Periority
+                      </InputLabel>
+                      <Select
+                        labelId='demo-simple-select-label'
+                        id='demo-simple-select'
+                        label='periority'
+                        name='periority'
+                        value={input.periority}
+                        onChange={handleInput}
+                      >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <button>Submit</button>
+                </Grid>
               </Grid>
             </Grid>
           </form>
@@ -101,7 +194,6 @@ const Form = () => {
         <Grid
           item
           xs={6}
-          style={{
             background: 'white',
             marginTop: '2rem',
             padding: '10px',
@@ -119,9 +211,10 @@ const Form = () => {
                     P
                   </Avatar>
                 </ListItemAvatar>
-                <ListItemText primary={task.title} />
+                <ListItemText primary={task.Task} secondary={task.Desc} />
               </ListItem>
               <ListItemSecondaryAction>
+                <p>Periority: {task.periority}</p>
                 <IconButton
                   style={{ color: 'green' }}
                   onClick={() => completedTask(task)}
@@ -159,9 +252,10 @@ const Form = () => {
                     P
                   </Avatar>
                 </ListItemAvatar>
-                <ListItemText primary={comTask.title} />
+                <ListItemText primary={comTask.Task} secondary={comTask.Desc} />
               </ListItem>
               <ListItemSecondaryAction>
+                <p>Periority: {comTask.periority}</p>
                 <IconButton
                   style={{ color: 'red' }}
                   onClick={() => deleteCompletedTasks(comTask)}
